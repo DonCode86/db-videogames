e2609ab16cbe6420308f9bc8b88c03015b2cf502
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-09-28 12:42:20 +0200

    point-3

---
+++ SQLQuery1.sql
@@ -0,0 +1,15 @@
+﻿-- 1- Selezionare tutte le software house americane (3)
+SELECT *
+FROM software_houses
+where country = 'united states';
+
+--2- Selezionare tutti i giocatori della città di 'Rogahnland' (2)
+SELECT * 
+FROM players 
+where city = 'Rogahnland';
+
+--3- Selezionare tutti i giocatori il cui nome finisce per "a" (220)
+
+SELECT *
+FROM players
+where name like '%a';
