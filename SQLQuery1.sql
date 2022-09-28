56a7395362b30dfe2a80ccdfe816d0cc0100cbf5
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-09-28 12:45:59 +0200

    point-4

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -9,7 +9,12 @@ FROM players
 where city = 'Rogahnland';
 
 --3- Selezionare tutti i giocatori il cui nome finisce per "a" (220)
-
 SELECT *
 FROM players
 where name like '%a';
+
+-- 4- Selezionare tutte le recensioni scritte dal giocatore con ID = 800 (11)
+
+SELECT *
+FROM reviews
+where player_id = 800;
