50b7cf131376f16fb4090b9188db6ad13824485b
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-09-28 13:26:23 +0200

    point-7

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -13,7 +13,8 @@ SELECT *
 FROM players
 where name like '%a';
 
--- 4- Selezionare tutte le recensioni scritte dal giocatore con ID = 800 (11)
+-- 4- Selezionare tutte le recensioni scritte dal 
+--giocatore con ID = 800 (11)
 
 SELECT *
 FROM reviews
@@ -25,8 +26,19 @@ SELECT *
 FROM tournaments
 where year = 2015;
 
---6- Selezionare tutti i premi che contengono nella descrizione la parola 'facere' (2)
+--6- Selezionare tutti i premi che contengono nella descrizione 
+--la parola 'facere' (2)
 
 SELECT *
 FROM awards
 where description like '%facere%';
+
+-- 7- Selezionare tutti i videogame che hanno 
+-- la categoria 2 (FPS) o 6 (RPG), 
+-- mostrandoli una sola volta (del videogioco vogliamo solo l'ID) (287)
+
+SELECT DISTINCT videogame_id
+FROM category_videogame 
+where category_id = 2 or category_id = 6
+
+-- 8- Selezionare tutte le recensioni con voto compreso tra 2 e 4 (2947)
